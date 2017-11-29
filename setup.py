ad2a1d558d9ceda55a2845a4e51c764e7d0906c9
Jeff Hill <jeffrey.p.hill@nasa.gov>
2017-11-28 22:33:00 -0800

    Add a basic setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from setuptools import setup
+setup(
+    name = 'basics',
+    version = '0.1.0',
+    descripion = 'Simple utilities for working in Python',
+    license = 'MIT',
+    url = 'https://github.com/flying-tiger/basics',
+    author = 'Jeffrey Hill',
+    author_email = 'jeff.p.hill@gmail.com',
+    packages = ['basics'],
+    python_requires = '>=3.6',
+)
